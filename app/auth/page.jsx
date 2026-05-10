f8c8b3279fc7307f93718eb739e6d46dc6b75d27
unknown <huzefaratlam63@gmail.com>
2026-05-10 18:29:22 +0530

    fixed signin spelling in auth page

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="flex h-full items-center justify-center">
       <div className="border border-(--highlightBorder) w-full max-w-md rounded-2xl bg-(--layer) p-8 shadow-lg">
-        <h1 className="text-xl font-bold text-center mb-5">Singin to continue with Tahfeez Dohad</h1>
+        <h1 className="text-xl font-bold text-center mb-5">signin to continue with Tahfeez Dohad</h1>
         <GoogleButton />
       </div>
     </div>
