74bd2c429aa680ebfb1f7c9656b6fb47a5527866
unknown <huzefaratlam63@gmail.com>
2026-05-13 13:39:53 +0530

    changed style in student card

--- app/_components/students/StudentCard.jsx
+++ app/_components/students/StudentCard.jsx
@@ -17,17 +17,17 @@ function StudentCard({ image, name, studentId }) {
   
   return (
     <div className="flex justify-center">
-      <div className="relative rounded-md w-3/4 border-t-4 border-amber-900 bg-(--layer)">
+      <div className="relative rounded-md w-3/4 border-t-4 border-amber-900 bg-(--layer) shadow border border-x-(--border) border-b-(--border)">
         <button
-        data-studentid={studentId}
-        data-studentname={name}
+          data-studentid={studentId}
+          data-studentname={name}
           className="menu-btn absolute right-2 top-2 w-8 flex justify-center py-1"
         >
           <BiDotsVertical className=" text-lg" />
         </button>
         <div
           // href={`/entry/${studentId}`}
-          className=" flex flex-col items-center  w-full p-4 rounded-md shadow border border-x-(--border) border-b-(--border) gap-2"
+          className=" flex flex-col items-center  w-full p-4 rounded-md  gap-2"
         >
           {image && (
             <div className="relative h-18 w-18 rounded-full overflow-hidden">
@@ -35,18 +35,17 @@ function StudentCard({ image, name, studentId }) {
             </div>
           )}
           {!image && <FaUserCircle className="text-5xl text-amber-950" />}
-          <p className="font-semibold text-stone-800 tracking-wider text-sm text-center">
+          <p className="font-semibold text-stone-800 tracking-wider text-xs text-center">
             {name}
           </p>
           <Link
             href={`/entry/${studentId}`}
             className="border rounded-full shadow-md p-2 bg-amber-800 text-white"
           >
-            <FaMicrophone />
+            <FaMicrophone className=""/>
           </Link>
         </div>
       </div>
-      
     </div>
   );
 }
