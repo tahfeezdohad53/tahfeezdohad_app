d0fdb4664982617d88ddb31939f39dce090bb121
unknown <huzefaratlam63@gmail.com>
2026-07-06 13:43:20 +0530

    in socket provider effect added isfetching as dependancy

--- app/_components/providers/SocketProvider.jsx
+++ app/_components/providers/SocketProvider.jsx
@@ -12,7 +12,7 @@ function SocketProvider({children}) {
     const [socket,setSocket] = useState();
      useEffect(() => {
       if(isFetching) return;
-      if(!user?._id){
+      if(!user?._id && !isFetching){
         if(socket) {
           // console.log('disconnecting socket cause no user id');
           socket.disconnect();
@@ -30,7 +30,7 @@ function SocketProvider({children}) {
       //    newSocket.disconnect();
       //    console.log("🔌 Socket disconnected from cleanup");
       //  };
-     }, [user?._id]);
+     }, [user?._id,isFetching]);
     return (
         <Context.Provider value={{socket}}>
             {children}
