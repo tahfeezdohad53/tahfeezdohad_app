f5f6e085b6705cff97c3b7f8afaadec8d7604da4
unknown <huzefaratlam63@gmail.com>
2026-07-19 18:45:27 +0530

    added download , forward and rewind options with native audio el in recordings

--- app/_components/RecordingEntry.jsx
+++ app/_components/RecordingEntry.jsx
@@ -1,15 +1,26 @@
 'use client';
 import { format } from "date-fns";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { CiCalendar } from "react-icons/ci";
 import { IoIosArrowDown } from "react-icons/io";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
+import { BiDotsVertical } from "react-icons/bi";
+import CustomContextMenu from "./CustomContextMenu";
+import { BsDownload } from "react-icons/bs";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { MdForward5 } from "react-icons/md";
+import { TbRewindBackward5, TbRewindForward5 } from "react-icons/tb";
 
 function RecordingEntry({el,i,isDummy=false}) {
     const [isExpand,setIsExpand] = useState(false);
+    const audioRef = useRef(null);
     if(!isDummy)return (
-      <div className="w-full hover:bg-(--card-hover) duration-300 ease-in-out transition-all hover:cursor-pointer" onClick={() => setIsExpand(!isExpand)}>
+      <div
+        className="w-full hover:bg-(--card-hover) duration-300 ease-in-out transition-all hover:cursor-pointer"
+        onClick={() => setIsExpand(!isExpand)}
+      >
         <div className="w-full grid grid-cols-9 lg:grid-cols-12 px-3 py-3 border-b border-(--border)">
           {/* <p>{(page - 1) * 10 + index + 1}</p> */}
           <div className="flex gap-2 items-center text-[0.55rem] tracking-wider lg:col-span-2 col-span-3">
@@ -27,12 +38,20 @@ function RecordingEntry({el,i,isDummy=false}) {
             {Math.round(el?.duration) || ""} min
           </p>
           <p className=" col-span-2 lg:col-span-4 text-[0.60rem] lg:text-sm text-left hyphens-auto">
-            {el.studentName.split(' ').length > 2 && el.studentName.split(' ').slice(1,el.studentName.split(' ').length).join(' ')}
-            {el.studentName.split(' ').length < 3 && el.studentName}
+            {el.studentName.split(" ").length > 2 &&
+              el.studentName
+                .split(" ")
+                .slice(1, el.studentName.split(" ").length)
+                .join(" ")}
+            {el.studentName.split(" ").length < 3 && el.studentName}
           </p>
           <p className=" col-span-2 lg:col-span-4 text-[0.60rem] lg:text-sm text-left truncate">
-            {el.teacherName.split(' ').length > 2 && el.teacherName.split(' ').slice(1,el.teacherName.split(' ').length).join(' ')}
-            {el.teacherName.split(' ').length < 3 && el.teacherName}
+            {el.teacherName.split(" ").length > 2 &&
+              el.teacherName
+                .split(" ")
+                .slice(1, el.teacherName.split(" ").length)
+                .join(" ")}
+            {el.teacherName.split(" ").length < 3 && el.teacherName}
           </p>
           <button
             // onClick={() => setIsExpand(!isExpand)}
@@ -44,15 +63,30 @@ function RecordingEntry({el,i,isDummy=false}) {
           </button>
         </div>
         {isExpand && (
-          <div onClick={(e) => e.stopPropagation()} className=" w-full flex justify-center py-2">
-            <div className="w-3/4 rounded-md overflow-hidden shadow-(--shadow-lg)">
-              <AudioPlayer
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="w-full flex justify-center py-2"
+          >
+            <div className="relative w-3/4 flex items-center gap-2">
+              {/* <AudioPlayer
+                customVolumeControls={[]}
                 autoPlay={false}
                 customAdditionalControls={[]}
                 src={el.audio}
                 // onPlay={() => console.log("Playing")}
                 className="text-sm"
-              />
+              /> */}
+              <audio
+              ref={audioRef}
+                src={el.audio}
+                className="w-full rounded-md  shadow-(--shadow-lg)"
+                controls
+              ></audio>
+              <div className="flex gap-2">
+                <button onClick={()=> audioRef.current.currentTime -= 5} className="bg-(--primary) text-white p-2 rounded-md hover:cursor-pointer duration-300 ease-in-out transition-all hover:bg-(--primary-light)"><TbRewindBackward5 className="text-xl"/></button>
+                <button onClick={() => audioRef.current.currentTime += 5} className="bg-(--primary) text-white p-2 rounded-md hover:cursor-pointer duration-300 ease-in-out transition-all hover:bg-(--primary-light)"><TbRewindForward5 className="text-xl"/></button>
+                
+              </div>
             </div>
           </div>
         )}
