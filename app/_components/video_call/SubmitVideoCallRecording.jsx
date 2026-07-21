c9c9a19917e0f2ace2e3d5cfe8e7793d5bf732cb
unknown <huzefaratlam63@gmail.com>
2026-07-21 15:12:18 +0530

    increase icon size for recording submit notification and increased duratio to 5 seconds

--- app/_components/video_call/SubmitVideoCallRecording.jsx
+++ app/_components/video_call/SubmitVideoCallRecording.jsx
@@ -31,7 +31,7 @@ function SubmitVideoCallRecording({
      setOnlineClassBlobUrl("");
     const toastId = "uploading";
     toast.success("your recording will be submitted, do not close or refresh browser before success notification arrives", {
-      icon: <AiOutlineExclamationCircle className="text-yellow-500"/>});
+      icon: <AiOutlineExclamationCircle className="text-yellow-500 text-2xl"/>, duration:5000});
     try {
       // console.log(data.signedUrl)
       const { data } = await axios.get(
