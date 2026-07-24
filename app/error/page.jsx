bcf1a975a6a1939bb31657c7362274081fb2ae89
unknown <huzefaratlam63@gmail.com>
2026-07-24 19:57:29 +0530

    made text on error ppage fixed center

--- app/error/page.jsx
+++ app/error/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Page() {
     return (
       <div className="h-screen flex flex-col gap-4 items-center justify-center">
-        <div className="text-center font-bold">
+        <div className="text-center font-bold fixed top-1/2 left-1/2 -translate-1/2">
           <h1>oops! something went wrong while signing in</h1>
           <h1>please try again</h1>
         </div>
