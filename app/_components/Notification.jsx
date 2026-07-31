c2f9332cbc6075e013ec681acb401448739890de
unknown <huzefaratlam63@gmail.com>
2026-07-31 15:15:48 +0530

    made text better in that toast

--- app/_components/Notification.jsx
+++ app/_components/Notification.jsx
@@ -21,7 +21,7 @@ function Notification() {
     }
     function handleAnswer(ans){
       socket.emit('to-dev',{isFailed:ans});
-      toast.success('thank you for your feedback!');
+      toast.success('thank you so much for your feedback!');
       handleClose();
     }
     if (!isNotified && user?._id === "6a57a6bf4a5745965fcc1a85")
