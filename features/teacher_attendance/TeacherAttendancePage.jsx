c921dc769bc49c8665d84456361c904c2cc68ab0
unknown <huzefaratlam63@gmail.com>
2026-09-12 13:54:08 +0530

    hided attendance status for admin

--- features/teacher_attendance/TeacherAttendancePage.jsx
+++ features/teacher_attendance/TeacherAttendancePage.jsx
@@ -12,7 +12,7 @@ function TeacherAttendancePage() {
   if(!user?._id) return null;
   return (
     <div className="p-5 px-2 flex flex-col gap-5">
-      <TodayAttendanceStatus />
+      {user?.role === 'teacher' && <TodayAttendanceStatus />}
 
       <TeacherAttendanceTable />
     </div>
