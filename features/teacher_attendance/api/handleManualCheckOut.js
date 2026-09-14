9880333d18899614f6140522f6f09b9d48f59f50
unknown <huzefaratlam63@gmail.com>
2026-09-14 19:04:43 +0530

    fixed timezone issue in manual checkout

--- features/teacher_attendance/api/handleManualCheckOut.js
+++ features/teacher_attendance/api/handleManualCheckOut.js
@@ -1,6 +1,5 @@
 import { api } from "@/lib/axios";
 
 export async function handleManualCheckout({checkOutDate,attendanceId,teacherId}) {
-    if (checkoutHour === "" || checkoutMinute === "") return;
         await api.patch('/teacherAttendance/manualCheckout',{checkOutDate,attendanceId,teacherId});
   }
